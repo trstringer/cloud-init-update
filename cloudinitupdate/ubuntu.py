920d607d520ebe6a04b644b7413074aa2070d116
Thomas Stringer <github@trstringer.com>
2019-08-07 13:37:03 -0400

    refactor upgrading code

--- cloudinitupdate/ubuntu.py
+++ cloudinitupdate/ubuntu.py
@@ -15,19 +15,12 @@ def check_and_update():
         None
     """
 
-    apt.Cache().update()
+    cache = apt.Cache()
+    cache.update()
 
-    ci_pkg = [
-        pkg
-        for pkg
-        in apt.Cache()
-        if pkg.shortname == _CLOUD_INIT_PKG_NAME
-    ]
-    if not ci_pkg:
-        raise Exception(f'{_CLOUD_INIT_PKG_NAME} not found')
-
-    if ci_pkg.is_upgradable:
+    if cache[_CLOUD_INIT_PKG_NAME].is_upgradable:
         print(f'{_CLOUD_INIT_PKG_NAME} is upgradable. Upgrading...')
-        ci_pkg.mark_upgrade()
+        cache[_CLOUD_INIT_PKG_NAME].mark_upgrade()
+        cache.commit()
     else:
         print(f'{_CLOUD_INIT_PKG_NAME} is not upgradable')
