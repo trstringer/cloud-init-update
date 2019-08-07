f378eafe40bc9de9ab8625c124d37c1de8feacad
Thomas Stringer <github@trstringer.com>
2019-08-07 10:20:58 -0400

    change implmentation to mark for upgrade

--- cloudinitupdate/ubuntu.py
+++ cloudinitupdate/ubuntu.py
@@ -15,33 +15,19 @@ def check_and_update():
         None
     """
 
-    ci_pkg_is_upgradable = _ci_pkg_is_upgradable()
-
-    if ci_pkg_is_upgradable is None:
-        raise Exception(f'{_CLOUD_INIT_PKG_NAME} not found')
-
-    if ci_pkg_is_upgradable:
-        print(f'{_CLOUD_INIT_PKG_NAME} is upgradable')
-    else:
-        print(f'{_CLOUD_INIT_PKG_NAME} is not upgradable')
-
-def _ci_pkg_is_upgradable():
-    """
-    Get the installed cloud-init package version.
-
-    Args:
-        None
-
-    Returns:
-        str: Package version of installed cloud-init.
-    """
-
     apt.Cache().update()
 
-    for pkg in apt.Cache():
-        if pkg.shortname == _CLOUD_INIT_PKG_NAME:
-            return pkg.is_upgradable
+    ci_pkg = [
+        pkg
+        for pkg
+        in apt.Cache()
+        if pkg.shortname == _CLOUD_INIT_PKG_NAME
+    ]
+    if not ci_pkg:
+        raise Exception(f'{_CLOUD_INIT_PKG_NAME} not found')
 
-    # This is unexpected, but in the event that cloud-init is not
-    # installed on the machine we should return nothing.
-    return None
+    if ci_pkg.is_upgradable:
+        print(f'{_CLOUD_INIT_PKG_NAME} is upgradable. Upgrading...')
+        ci_pkg.mark_upgrade()
+    else:
+        print(f'{_CLOUD_INIT_PKG_NAME} is not upgradable')
