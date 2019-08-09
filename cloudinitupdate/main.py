939b0eee206449389602457d0aad9cd4ba06c3ce
Thomas Stringer <github@trstringer.com>
2019-08-09 17:32:01 -0400

    refactor to auto discover distro

--- cloudinitupdate/main.py
+++ cloudinitupdate/main.py
@@ -1,25 +1,16 @@
 """Main code path for cloud-init-update."""
 
-import argparse
-from .ubuntu import check_and_update
+import distro
+from .apt import check_and_update as apt_check_and_update
 
 def main():
     """Main code execution."""
 
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument(
-        '-d', '--distro',
-        help='Current distribution',
-        default='ubuntu'
-    )
-
-    args = parser.parse_args()
-
-    print(f'Distro: {args.distro}')
-
-    if args.distro.lower() == 'ubuntu':
-        check_and_update()
+    linux_distro = distro.linux_distribution()
+    if linux_distro[0].lower() in ['ubuntu', 'debian']:
+        apt_check_and_update()
+    else:
+        raise Exception(f'Unknown distro: {linux_distro}')
 
 if __name__ == '__main__':
     main()
